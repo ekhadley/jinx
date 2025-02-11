c2b264dd5cfb3b204ec021cdea8b6d8bfa86525f
Ethan Hadley <ekhadley@gmail.com>
2025-02-11 15:15:17 -0600

    Asd

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,40 @@
 const std = @import("std");
-const File = std.fs.File;
 const print = std.debug.print;
+const MAX_BUFFER_SIZE = 1024;
+
+const escape_code = "\x1b";
+const home_code = escape_code ++ "[H";
+
+const ScreenBuffer = struct {
+    const len = MAX_BUFFER_SIZE;
+    var place: usize = 0;
+    var contents = [MAX_BUFFER_SIZE]u8{};
+    pub fn init() ScreenBuffer {
+        return .{};
+    }
+    pub fn clear(self: ScreenBuffer) !void {
+        for (self.contents[self.place .. self.place + escape_code.len], escape_code) |*d, s| {
+            d.* = s;
+        }
+        self.place += escape_code;
+    }
+};
+
+pub fn writeBuffer(tty: std.fs.File, buf: ScreenBuffer) !void {
+    try tty.write(buf.contents[0..buf.place]);
+}
 
 pub fn main() !void {
     //var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     //defer _ = gpa.deinit();
     //const alloc = gpa.allocator();
-
     const tty = try std.fs.openFileAbsolute("/dev/tty", .{ .mode = .write_only, .allow_ctty = true });
-    //const tty = try std.fs.openFileAbsolute("/home/ek/wgmn/jinx/hi.txt", .{});
     defer tty.close();
-    _ = try tty.write("123abc\n");
+
+    const buf = ScreenBuffer.init();
+    print("buf.size = {d}, buf.place = {d}", .{ buf.len, buf.place });
+    //buf.clear();
+    //writeBuffer(tty, buf);
+
+    while (true) {}
 }
