f286f8cb965b9a2b6ce36edd1f4af62be7354d83
Ethan Hadley <ekhadley@gmail.com>
2025-08-25 09:02:43 -0500

    build opts

--- build.zig
+++ build.zig
@@ -11,46 +11,25 @@ pub fn build(b: *std.Build) void {
     });
     b.installArtifact(lib);
 
-    const exe = b.addExecutable(.{
-        .name = "jinx",
+    const demo_exe = b.addExecutable(.{
+        .name = "demo",
         .root_source_file = b.path("src/demo.zig"),
         .target = target,
         .optimize = optimize,
     });
-    b.installArtifact(exe);
-    const demo_cmd = b.addRunArtifact(exe);
+    b.installArtifact(demo_exe);
+    const demo_cmd = b.addRunArtifact(demo_exe);
     demo_cmd.step.dependOn(b.getInstallStep());
-<<<<<<< Updated upstream
 
-    const snake_exe = b.addExecutable(.{
-        .name = "jinx",
-        .root_source_file = b.path("src/snake.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(snake_exe);
-    const snake_cmd = b.addRunArtifact(exe);
-    snake_cmd.step.dependOn(b.getInstallStep());
-
-    const run_demo_step = b.step("demo", "Run the demo");
-    run_demo_step.dependOn(&demo_cmd.step);
-
-    const run_snake_step = b.step("snake", "Run the snake game demo");
-    run_snake_step.dependOn(&demo_cmd.step);
-=======
-    const run_step = b.step("demo", "Run the demo");
-    run_step.dependOn(&demo_cmd.step);
-
-    const exe2 = b.addExecutable(.{
+    const input_demo_exe = b.addExecutable(.{
         .name = "input_demo",
         .root_source_file = b.path("src/input_demo.zig"),
         .target = target,
         .optimize = optimize,
     });
-    b.installArtifact(exe2);
-    const input_demo_cmd = b.addRunArtifact(exe2);
+    b.installArtifact(input_demo_exe);
+    const input_demo_cmd = b.addRunArtifact(input_demo_exe);
     input_demo_cmd.step.dependOn(b.getInstallStep());
     const input_demo_run_step = b.step("input_demo", "Run the terminal input demo");
     input_demo_run_step.dependOn(&input_demo_cmd.step);
->>>>>>> Stashed changes
 }
